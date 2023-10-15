d1b929cb16169c2dc20528d25d19598fb684ddeb
Adam Hussein <60454449+adamhusse818@users.noreply.github.com>
2023-10-15 02:53:16 -0400

    Update Pipe.java

--- src/Pipe.java
+++ src/Pipe.java
@@ -1,30 +1,41 @@
 /**
- * Class for designing the pipes
- * Each instance will generate two pipes: one that will face up and one that will face down
- * The distance between each pipe will be fair so the user will not rage-quit
+ * Class for designing the pipes and their behaviour
+ * The class will generate one pipe
+ * The second one is made by flipping the pipe
  */
 
+// Import statements
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.Random;
 
 public class Pipe implements ActionListener{
-    Image pipe;
-    Timer timer;
+    final String spritePath = "Assets/Sprites/"; // Use this for all file paths for sprites
+    Image pipe; // Image of the pipe
+    Timer timer; // Timer used for moving the pipe
     int xPipe; // Initial x position of every pair of pipes
-    int yTopPipe;
-    boolean passed;
+    int yTopPipe; // Y position of the top pipe. Used for separation
+    boolean passed; // Keep track of whether the bird has passed the pair of pipes
     Pipe(){
-        this.pipe = new ImageIcon("Assets/Sprites/PipeGreen.png").getImage();
+        this.pipe = new ImageIcon(spritePath + "PipeGreen.png").getImage();
+
+        // Use the random class to generate a random y value for the pipe
         Random rand = new Random();
         this.yTopPipe = -rand.nextInt(150);
+
+        // All the pipes will start at the same x value
         this.xPipe = 330;
-        passed = false;
-        timer = new Timer(30, this);
+        this.passed = false;
+        this.timer = new Timer(30, this);
     }
 
+    /**
+     * Method used to move the pair of pipes to the left
+     * This method is invoked every 30 milliseconds due to the timer
+     * @param e the event to be processed
+     */
     public void actionPerformed(ActionEvent e){
-        xPipe -= 5;
+        this.xPipe -= 5;
     }
 }
