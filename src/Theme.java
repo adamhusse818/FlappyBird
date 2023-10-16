88a20a2e01b3ef6c454822562b7eac290e19c84b
Adam Hussein <60454449+adamhusse818@users.noreply.github.com>
2023-10-16 19:16:05 -0400

    Add files via upload

---
+++ src/Theme.java
@@ -0,0 +1,30 @@
+/**
+ * Class used for making the backgrounds
+ */
+
+// Import statements
+import javax.swing.ImageIcon;
+import java.awt.Image;
+
+class Theme{
+    Image theme; // Used to store image of background;
+    private int themeIndex; // Keep track of the index of the current theme
+    private String[] themes = {"Day", "Night"}; // Array of the backgrounds available
+    private final String SPRITE_PATH = "Assets/Sprites/Background"; // Use this for all file paths for sprites
+
+    Theme(String theme){
+        this.theme = new ImageIcon(SPRITE_PATH + theme + ".png").getImage();
+        this.themeIndex = 0;
+    }
+
+    void changeTheme(){
+        if(this.themeIndex == themes.length - 1){
+            this.themeIndex = 0;
+            this.theme = new ImageIcon(SPRITE_PATH + themes[0] + ".png").getImage();
+        }
+        else{
+            this.theme = new ImageIcon(SPRITE_PATH + themes[themeIndex + 1] + ".png").getImage();
+            this.themeIndex++;
+        }
+    }
+}
