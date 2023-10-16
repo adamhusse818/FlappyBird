819486ac4d8332d0f2d6702a7e713e02c6046bcf
Adam Hussein <60454449+adamhusse818@users.noreply.github.com>
2023-10-16 19:15:07 -0400

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -1,32 +1,29 @@
 /**
- * Class for designing the interface for actually playing
- * It will use a new panel to display the game
- * This class extends the javax.swing.JPanel class
+ * Class for designing the interface of the panel that is added to the frame
+ * The entire structure of the game is handled here
+ * TO-DO: Implement file operation that saves player high score, bird color, and background
  */
 
 import javax.swing.*;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
+import javax.sound.sampled.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.File;
 
-public class Game extends JPanel implements ActionListener, MouseListener{
-    Image background, base, readyMessage, gameOverMessage, endScreen; // Images associated with the gameplay
-    int xBase = 0; // Initial x position of the ground
-    Timer timer; // Used for motion of background
-    Bird bird; // Bird object
-    Pipe[] pipes;
-    boolean gameStarted;
-    boolean gameOver;
-    boolean mouseEnabled = true;
-    int score = 0;
-    int y = 600;
-    int screenVelocity = 20;
-    Button birdButton, themeButton, restartButton;
+class Game extends JPanel implements ActionListener, MouseListener{
+    private final String SPRITE_PATH = "Assets/Sprites/"; // Use this for all sprites
+    private Image base, readyMessage, gameOverMessage, endScreen; // Images associated with the gameplay
+    private int xBase; // Initial x position of the ground
+    private Bird bird; // Bird object
+    private Pipe[] pipes; // Array used to store the pairs of pipes
+    private boolean gameStarted, gameOver; // Different states of the game
+    private int score; // Score of the user
+    private int yEndMessage; // Used for displaying the end message
+    private int screenVelocity; // Velocity associated with the end screen
+    private Button birdButton, themeButton, restartButton; // Buttons for different purposes
+    private Theme background; // The background used for the game
 
-    ImageIcon[] nums = {
+    private ImageIcon[] numImages = {
             new ImageIcon("Assets/Sprites/0.png"),
             new ImageIcon("Assets/Sprites/1.png"),
             new ImageIcon("Assets/Sprites/2.png"),
@@ -40,32 +37,39 @@ public class Game extends JPanel implements ActionListener, MouseListener{
     };
 
     Game(){
+        // Some initializations
+        screenVelocity = 20;
+        yEndMessage = 600;
+        score = 0;
+        xBase = 0;
+
         // Initial setting up for the panel
         this.setLayout(null);
         this.setPreferredSize(new Dimension(336, 612));
         this.addMouseListener(this);
 
-        // Setting up the Flappy Bird environment by creating the background and message
-        background = new ImageIcon("Assets/Sprites/BackgroundNight.png").getImage();
-        base = new ImageIcon("Assets/Sprites/Base.png").getImage();
-        readyMessage = new ImageIcon("Assets/Sprites/Message.png").getImage();
-
-        gameOverMessage = new ImageIcon("Assets/Sprites/GameOver.png").getImage();
-        endScreen = new ImageIcon("Assets/Sprites/EndScreen.png").getImage();
+        // Setting up the Flappy Bird environment by creating the themes, screens, and messages
+        background = new Theme("Day");
+        base = new ImageIcon(SPRITE_PATH + "Base.png").getImage();
+        readyMessage = new ImageIcon(SPRITE_PATH + "Message.png").getImage();
+        gameOverMessage = new ImageIcon(SPRITE_PATH + "GameOver.png").getImage();
+        endScreen = new ImageIcon(SPRITE_PATH + "EndScreen.png").getImage();
 
         // Initialize the bird
-        bird = new Bird();
+        bird = new Bird("Yellow");
 
+        // Create button for changing bird color
         birdButton = new Button("Bird Color", 25, 435);
         birdButton.addActionListener(e -> {
             if(e.getSource() == birdButton){
                 bird.changeColor();
             }
         });
+        this.add(birdButton);
 
+        // Creating button to allow user to restart after dying
         restartButton = new Button("Restart", 120, 435);
         restartButton.disableButton();
-        this.add(restartButton);
         restartButton.addActionListener(e -> {
             if(e.getSource() == restartButton){
                 playSound("Swoosh");
@@ -73,44 +77,61 @@ public class Game extends JPanel implements ActionListener, MouseListener{
             }
             restartButton.disableButton();
         });
+        this.add(restartButton);
 
-        this.add(birdButton);
-
+        // Creating button to allow user to change background
         themeButton = new Button("Theme", 200, 435);
-
+        themeButton.addActionListener(e -> {
+            if(e.getSource() == themeButton){
+                background.changeTheme();
+            }
+        });
         this.add(themeButton);
 
+        /*
+         Initializing the array for the pipes
+         The list will have 2 pipes at a time
+         A new pipe is added whenever one passes the left side of the panel
+         This allows for the re-generation of pipes
+         */
         pipes = new Pipe[2];
         pipes[0] = new Pipe();
         pipes[1] = new Pipe();
-        pipes[1].xPipe = pipes[0].xPipe + 200;
+        pipes[1].xPipe = pipes[0].xPipe + 200; // A pair of pipes will have 200 pixels of space in between the next pair
 
+        // Initialize our states of the game
         gameStarted = false;
         gameOver = false;
 
-        // Initialize timers
-        timer = new Timer(30, this);
+        // Initialize timer
+        // Used for motion of theme
+        Timer timer = new Timer(30, this);
         timer.start();
     }
 
     /**
-     *
+     * Method used for painting the entire game
+     * This will determine which state the game is in, then go from there
      * @param g the <code>Graphics</code> object to protect
      */
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         Graphics2D g2D = (Graphics2D) g;
-        g2D.drawImage(background, 0, 0, null);
+        g2D.drawImage(background.theme, 0, 0, null);
+        if(!gameStarted && !gameOver){
+            g2D.drawImage(readyMessage, 75, 50, null);
+        }
         if(gameStarted){
             birdButton.disableButton();
             themeButton.disableButton();
             for(Pipe pipe : pipes){
-                Image p = pipe.pipe;
+                Image pImage = pipe.pipe;
                 int x = pipe.xPipe;
                 int y = pipe.yTopPipe;
                 pipe.timer.start();
-                g2D.drawImage(p, x, y + p.getHeight(null) + 100, null);
-                g2D.drawImage(p, x, y + p.getHeight(null), p.getWidth(null), -p.getHeight(null), null);
+                g2D.drawImage(pImage, x, y + pImage.getHeight(null) + 100, null); // Bottom Pipe
+                g2D.drawImage(pImage, x, y + pImage.getHeight(null), pImage.getWidth(null),
+                        -pImage.getHeight(null), null); // Bottom pipe flipped -> top pipe
                 if(x <= 48 && !pipe.passed){
                     score++;
                     pipe.passed = true;
@@ -118,38 +139,35 @@ public class Game extends JPanel implements ActionListener, MouseListener{
                 }
             }
             if(!gameOver){
-                int tempScore = score;
-                for(int i = 0; i < String.valueOf(score).length(); i++){
-                    int digit = tempScore % 10;
-                    Image digitImage = nums[digit].getImage();
-                    g2D.drawImage(digitImage, 152 - (20 * i), 50, null);
-                    tempScore /= 10;
+                String tempScore = String.valueOf(score); // For organization purposes, score is converted to String
+                for(int i = 0; i < tempScore.length(); i++){
+                    String digit = tempScore.substring(i, i + 1);
+                    Image digitImage = numImages[Integer.parseInt(digit)].getImage();
+                    g2D.drawImage(digitImage, 152 + (20 * i), 50, null);
                 }
             }
         }
-        else if(!gameOver){
-            g2D.drawImage(readyMessage, 75, 50, null);
-        }
-        if(gameOver){
+       if(gameOver){
             g2D.drawImage(gameOverMessage, 75, 150, null);
-            g2D.drawImage(endScreen, 20, y, null);
-            int tempScore = score;
+            g2D.drawImage(endScreen, 20, yEndMessage, null);
+            String tempScore = String.valueOf(score);
             for(int i = 0; i < String.valueOf(score).length(); i++){
-                int digit = tempScore % 10;
-                Image digitImage = nums[digit].getImage();
-                g2D.drawImage(digitImage, 240 - (20 * i), y + 45, null);
-                tempScore /= 10;
+                String digit = tempScore.substring(i, i + 1);
+                Image digitImage = numImages[Integer.parseInt(digit)].getImage();
+                g2D.drawImage(digitImage, 240 + (20 * i), yEndMessage + 45, null);
             }
             String medal = determineMedal();
             if(medal != null){
                 Image medalImage = new ImageIcon(medal).getImage();
-                g2D.drawImage(medalImage, 55, y + 55, null);
+                g2D.drawImage(medalImage, 55, yEndMessage + 55, null);
             }
             restartButton.enableButton();
         }
-        g2D.drawImage(bird.getCurrentWingPos(), 90, bird.yBird, null);
-        g2D.drawImage(base, xBase, background.getHeight(null), null);
-        g2D.drawImage(base, xBase + base.getWidth(null), background.getHeight(null), null);
+
+       // Drawing what is there no matter the state of the game
+        g2D.drawImage(bird.currentWingPos, bird.xBird, bird.yBird, null);
+        g2D.drawImage(base, xBase, background.theme.getHeight(null), null);
+        g2D.drawImage(base, xBase + base.getWidth(null), background.theme.getHeight(null), null);
     }
 
     /**
@@ -163,48 +181,54 @@ public class Game extends JPanel implements ActionListener, MouseListener{
                 xBase = 0;
             }
             xBase -= 5;
-            if(gameStarted){
-                if(pipes[0].xPipe <= -pipes[0].pipe.getWidth(null)){
-                    pipes[0].timer.stop();
-                    pipes[0] = pipes[1];
-                    pipes[1] = new Pipe();
-                }
-                bird.velocity += bird.acceleration;
-                bird.yBird += bird.velocity;
-                collisionTest();
-            }
         }
-        else{
+        if(gameStarted && !gameOver){
+            if(pipes[0].xPipe <= -pipes[0].pipe.getWidth(null)){
+                pipes[0].timer.stop();
+                pipes[0] = pipes[1];
+                pipes[1] = new Pipe();
+            }
+            bird.velocity += bird.acceleration;
+            bird.yBird += bird.velocity;
+            collisionTest();
+        }
+        if(gameOver){
             for(Pipe p : pipes){
                 p.timer.stop();
             }
             bird.timer.stop();
-            if(background.getHeight(null) - (bird.yBird + 24) <= 1){
+            if(background.theme.getHeight(null) - (bird.yBird + 24) <= 1){
                 bird.velocity = 0;
                 bird.acceleration = 0;
             }
             bird.velocity += bird.acceleration;
             bird.yBird += bird.velocity;
-            if(y == 200){
+            if(yEndMessage == 200){
                 screenVelocity = 0;
             }
-            y -= screenVelocity;
+            yEndMessage -= screenVelocity;
         }
         repaint();
     }
 
-    public void collisionTest(){
-        if(background.getHeight(null) - (bird.yBird + 24) <= 1){
+    /**
+     * This function tests if the bird collided with a pipe or the ground
+     * If it does, we end the game and play the lovely sound of death
+     */
+    void collisionTest(){
+        /*
+         Since the physics don't always allow the bird to be exactly at a certain point,
+         we test if the bird is within a certain amount of pixels to the area
+         */
+        if(background.theme.getHeight(null) - (bird.yBird + 24) <= 1){
             bird.velocity = 0;
             bird.acceleration = 0;
-            mouseEnabled = false;
             gameOver = true;
             playSound("Hit");
         }
         for(Pipe p : pipes){
             if(!p.passed && ((bird.yBird <= (p.yTopPipe + 320) || bird.yBird >= (p.yTopPipe + 396))
                     && (p.xPipe >= 56 && p.xPipe <= 124))){
-                mouseEnabled = false;
                 gameOver = true;
                 playSound("Hit");
                 playSound("Die");
@@ -212,25 +236,33 @@ public class Game extends JPanel implements ActionListener, MouseListener{
         }
     }
 
-    public void restart(){
+    /**
+     * This function restarts the game by resetting the variables and objects
+     */
+    void restart(){
         gameOver = false;
         gameStarted = false;
-        mouseEnabled = true;
         birdButton.enableButton();
         themeButton.enableButton();
-        bird = new Bird();
+        bird = new Bird("Yellow");
         pipes[0].timer.stop();
         pipes[1].timer.stop();
         pipes[0] = new Pipe();
         pipes[1] = new Pipe();
         pipes[1].xPipe = pipes[0].xPipe + 200;
-        y = 600;
+        yEndMessage = 600;
         screenVelocity = 20;
         score = 0;
     }
 
-    public String determineMedal(){
-        String medal = "Assets/Sprites/";
+    /**
+     * This function determines which medal the user earns
+     * 10 pts is bronze, 20 pts is silver,
+     * 30 pts is gold, 40 pts is platinum
+     * @return a String for the medal that is earned by the user
+     */
+    String determineMedal(){
+        String medal = SPRITE_PATH;
         if(score >= 40){
             medal += "MedalPlatinum.png";
         }
@@ -249,10 +281,14 @@ public class Game extends JPanel implements ActionListener, MouseListener{
         return medal;
     }
 
-    public static void playSound(String soundName){
-        String path = "Assets/Audio/";
-        String extension = ".wav";
-        String soundFile = path + soundName + extension;
+    /**
+     * This function plays a sound
+     * If the sound does not exist, we messed up and an error is thrown
+     * @param soundName a string for the sound that is played
+     */
+    static void playSound(String soundName){
+        String audioPath = "Assets/Audio/";
+        String soundFile = audioPath + soundName + ".wav";
         try{
             Clip clip = AudioSystem.getClip();
             AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File(soundFile).getAbsoluteFile());
@@ -266,11 +302,17 @@ public class Game extends JPanel implements ActionListener, MouseListener{
 
     @Override
     public void mouseClicked(MouseEvent e){
+        // NOT USED
     }
 
+    /**
+     * Used to start the game and make the bird jump
+     * This is only invoked if the mouse is enabled
+     * @param e the event to be processed
+     */
     @Override
     public void mousePressed(MouseEvent e){
-        if(mouseEnabled){
+        if(!gameOver){
             gameStarted = true;
             bird.jump();
         }
@@ -278,13 +320,16 @@ public class Game extends JPanel implements ActionListener, MouseListener{
 
     @Override
     public void mouseReleased(MouseEvent e){
+        // NOT USED
     }
 
     @Override
     public void mouseEntered(MouseEvent e){
+        // NOT USED
     }
 
     @Override
     public void mouseExited(MouseEvent e){
+        // NOT USED
     }
 }
