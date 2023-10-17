ca96ab357e18a70f26dcfa294933eb74757fb871
Adam Hussein <60454449+adamhusse818@users.noreply.github.com>
2023-10-17 01:00:35 -0400

    Added ability to save bird color, high score, and theme

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -3,18 +3,21 @@
  * The frame will incorporate a panel that displays game
  * A main method is included here. A new "main" file is NOT required
  * This file is used to run the game. Simply run it, and you should be able to play
- * TO-DO: Implement file operation that saves player high score, bird color, and background
  */
 
 // Import statements
 import javax.swing.JFrame;
 import javax.swing.ImageIcon;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.io.FileWriter;
+import java.io.IOException;
 
-public class FlappyBird extends JFrame{
-    FlappyBird(){
+public class FlappyBird extends JFrame implements WindowListener{
+    public FlappyBird(){
         // Setting up the frame. Add title, change icon, set characteristics, etc.
         this.setTitle("Flappy Bird");
-        
+
         // Use this for all file paths for sprites
         String spritePath = "Assets/Sprites/";
         this.setIconImage(new ImageIcon(spritePath + "YellowBirdMidFlap.png").getImage());
@@ -26,9 +29,65 @@ public class FlappyBird extends JFrame{
 
         // Add the panel to the frame, pack its contents, set location on screen, then set it to be visible
         this.add(panel);
+        this.addWindowListener(this);
         this.pack();
         this.setLocationRelativeTo(null);
         this.setVisible(true);
+    }
+
+    @Override
+    public void windowOpened(WindowEvent e){
+        // NOT USED
+    }
+
+    /**
+     * Function used to determine if the window closes
+     * We use this to write the user data
+     * @param e the event to be processed
+     */
+    @Override
+    public void windowClosing(WindowEvent e){
+        try{
+            FileWriter fw = new FileWriter("Data.txt");
+            String toWrite = "";
+            for(int i = 0; i < 3; i++){
+                if(i == 2){
+                    toWrite += Game.data[i];
+                }
+                else{
+                    toWrite += Game.data[i] + ",";
+                }
+            }
+            fw.write(toWrite);
+            fw.close();
+        }catch(IOException a){
+            System.out.println(a.getMessage());
+        }
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e){
+        // NOT USED
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e){
+        // NOT USED
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent e){
+        // NOT USED
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e){
+        // NOT USED
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e){
+        // NOT USED
     }
 
     public static void main(String[] args){
