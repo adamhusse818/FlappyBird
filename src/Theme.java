0a69b91ad35edd68c15bad7c398c2cc885e7de27
Adam Hussein <60454449+adamhusse818@users.noreply.github.com>
2023-10-16 23:32:09 -0400

    Made public

--- src/Theme.java
+++ src/Theme.java
@@ -1,23 +1,23 @@
 /**
- * Class used for making the backgrounds
+ * Class used for making the theme of the background
  */
 
 // Import statements
 import javax.swing.ImageIcon;
 import java.awt.Image;
 
-class Theme{
+public class Theme{
     Image theme; // Used to store image of background;
     private int themeIndex; // Keep track of the index of the current theme
     private String[] themes = {"Day", "Night"}; // Array of the backgrounds available
     private final String SPRITE_PATH = "Assets/Sprites/Background"; // Use this for all file paths for sprites
 
-    Theme(String theme){
+    public Theme(String theme){
         this.theme = new ImageIcon(SPRITE_PATH + theme + ".png").getImage();
         this.themeIndex = 0;
     }
 
-    void changeTheme(){
+    public void changeTheme(){
         if(this.themeIndex == themes.length - 1){
             this.themeIndex = 0;
             this.theme = new ImageIcon(SPRITE_PATH + themes[0] + ".png").getImage();
